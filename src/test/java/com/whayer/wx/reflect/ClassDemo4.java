6a5f3f4ba63169bd186889a228e2f08c7569ec51
duyud <duyud@qq.com>
2017-03-12 22:29:04 +0800

    add reflect unit test

---
+++ src/test/java/com/whayer/wx/reflect/ClassDemo4.java
@@ -0,0 +1,16 @@
+package com.whayer.wx.reflect;
+
+public class ClassDemo4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassUtil.printFieldMessage("hello");
+		System.out.println("=============");
+		ClassUtil.printFieldMessage(new Integer(1));
+
+	}
+
+}
---
+++ src/test/java/com/whayer/wx/reflect/ClassDemo5.java
@@ -0,0 +1,16 @@
+package com.whayer.wx.reflect;
+
+public class ClassDemo5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassUtil.printConMessage("hello");
+		ClassUtil.printConMessage(new Integer(1));
+		
+
+	}
+
+}
