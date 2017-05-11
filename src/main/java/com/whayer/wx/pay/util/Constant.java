95326c98cc0e1ad47af084da0e44877a6c71861f
duyu <duyud@qq.com>
2017-05-11 16:49:17 +0800

    fix

--- src/main/java/com/whayer/wx/pay/util/Constant.java
+++ src/main/java/com/whayer/wx/pay/util/Constant.java
@@ -4,15 +4,15 @@ public class Constant {
 	public static final String DOMAIN = "https://scskss.com";
 
 	//唯一id
-    public static final String APP_ID = "wx2e4ffffd52348193"; 
+    public static final String APP_ID = ""; 
 
     //应用密匙(API接口密码)
-    public static final String APP_SECRET = "5fb1312de6c4ec7f6433fc37efae20ad"; 
+    public static final String APP_SECRET = ""; 
 
     //签名密钥
-    public static final String APP_KEY = "4QwPY2JcpDCzaxWBZtsK49DiOKTlYTxO"; 
+    public static final String APP_KEY = ""; 
 
-    public static final String MCH_ID = "1465571902";  //商户号
+    public static final String MCH_ID = "";  //商户号
 
     public static final String URL_UNIFIED_ORDER = "https://api.mch.weixin.qq.com/pay/unifiedorder";
     
