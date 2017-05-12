80a75be67b40a556bef7a26e047cadf80ad71e3e
duyu <duyud@qq.com>
2017-05-12 08:15:37 +0800

    fix

--- src/main/java/com/whayer/wx/pay/util/Constant.java
+++ src/main/java/com/whayer/wx/pay/util/Constant.java
@@ -1,18 +1,18 @@
 package com.whayer.wx.pay.util;
 
 public class Constant {
-	public static final String DOMAIN = "https://scskss.com";
+	public static final String DOMAIN = "m";
 
 	//唯一id
-    public static final String APP_ID = "wx2e4ffffd52348193"; 
+    public static final String APP_ID = ""; 
 
     //应用密匙(API接口密码)
-    public static final String APP_SECRET = "5fb1312de6c4ec7f6433fc37efae20ad"; 
+    public static final String APP_SECRET = ""; 
 
     //签名密钥
-    public static final String APP_KEY = "4QwPY2JcpDCzaxWBZtsK49DiOKTlYTxO"; 
+    public static final String APP_KEY = ""; 
 
-    public static final String MCH_ID = "1465571902";  //商户号
+    public static final String MCH_ID = "";  //商户号
 
     public static final String URL_UNIFIED_ORDER = "https://api.mch.weixin.qq.com/pay/unifiedorder";
     
@@ -20,9 +20,9 @@ public class Constant {
     
     public static final String URL_ORDER_QUERY = "https://api.mch.weixin.qq.com/pay/orderquery";
 
-    public static final String URL_NOTIFY = DOMAIN + "/payV2/callback"; 
+    public static final String URL_NOTIFY = DOMAIN + ""; 
     
-    public static final String URL_BARCODE = DOMAIN + "/gift/gift.html"; 
+    public static final String URL_BARCODE = DOMAIN + ""; 
 
     public static final String TIME_FORMAT = "yyyyMMddHHmmss";
 
