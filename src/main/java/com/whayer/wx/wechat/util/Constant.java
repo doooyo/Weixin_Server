a49eefbc191fcb51c7c1ee8ea377cd8feefe3d1e
duyu <duyud@qq.com>
2017-07-25 01:54:30 +0800

    delete some file

--- src/main/java/com/whayer/wx/wechat/util/Constant.java
+++ src/main/java/com/whayer/wx/wechat/util/Constant.java
@@ -4,13 +4,13 @@ import com.whayer.wx.wechat.vo.AccessToken;
 
 public class Constant {
 
-	public static final String APPID = "wx1fa5a2f6ba043f0c"; //"wxe6d4623ad9018e7e";
-	public static final String APPSECRET = "ac9a32235b65545cbb08764f5b3a631a"; //"b371c2eb4c348828367ab949ee7d4f39";
-	public static final String TOKEN = "scskss";
+	public static final String APPID = ""; //"wxe6d4623ad9018e7e";
+	public static final String APPSECRET = ""; //"b371c2eb4c348828367ab949ee7d4f39";
+	public static final String TOKEN = "";
 	
-	public static final String ORIGINALID = "gh_af6d9f826816";//minipro
+	public static final String ORIGINALID = "";//minipro
 	
-	public static final String ACCESS_TOKEN_URL =  "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+	public static final String ACCESS_TOKEN_URL =  "";
 	
 	
 	public static AccessToken ACCESS_TOKEN = null;
